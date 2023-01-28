2332b8ab36d75269288f8b4f28fb72fdc13598eb
knoxg <knoxg@excimer.mshome.net>
2023-01-29 08:40:06 +1000

    copy bin files

--- openssl-ca/bin/init-openssl-ca.sh
+++ openssl-ca/bin/init-openssl-ca.sh
@@ -1,5 +1,7 @@
 echo "Creating directories under /opt/openssl-ca"
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 sudo mkdir -p /opt/openssl-ca
 sudo chown $USER:$USER /opt/openssl-ca
 cd /opt/openssl-ca
@@ -9,3 +11,5 @@ mkdir private
 mkdir csr
 mkdir ext
 mkdir cert 
+
+cp ${SCRIPT_DIR}/bin/* /opt/openssl-ca/bin
